9275f43d8a8f3cca0abab48ff7cf5aa268b5b1d5
yossi <yossi@sears.co.il>
2017-11-14 20:50:04 +0200

    Complete blockchain class

--- blockchain.py
+++ blockchain.py
@@ -1,18 +1,58 @@
+from hashlib import sha256
+import json
+from time import time
+from uuid import uuid4
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
-    def new_block(self):
-        pass
+        self.new_block(previous_hash=1, proof=100)
 
-    def new_transaction(self):
-        pass
+    def new_block(self, previous_hash, proof):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash
+        }
+        
+        self.current_transactions = []
+
+        self.chain.append(block)
+
+        return block
+
+    def new_transaction(self, sender, recipient, amount):
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        })
+
+        return self.last_block['index'] + 1
 
     @staticmethod
     def hash(block):
-        pass
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
-        pass
+        return self.chain[-1]
+
+    def proof_of_work(self, last_proof):
+        proof = 0
+
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
