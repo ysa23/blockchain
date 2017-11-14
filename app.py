1a24fb6bc8a54022800f45a376816fb4e1d47416
yossi <yossi@sears.co.il>
2017-11-14 21:10:31 +0200

    Add initial web server

--- app.py
+++ app.py
@@ -1,1 +1,31 @@
-print 'Hello their!'
+from textwrap import dedent
+from flask import Flask
+from uuid import uuid4
+import json
+from blockchain import Blockchain
+
+app = Flask(__name__)
+
+node_identifier = str(uuid4()).replace('-', '')
+
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "We'll mine a new block"
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    return "We'll add a new transaction"
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
